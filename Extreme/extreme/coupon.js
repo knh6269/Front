a50b6c17d741911e9cfa34f869e825a8e4f44949
knh6269 <knh6269@gmail.com>
2021-11-09 16:21:52 +0900

    d

--- Extreme/extreme/coupon.js
+++ Extreme/extreme/coupon.js
@@ -1,1 +1,43 @@
+import React from 'react'
+import {
+    Dimensions,} from "react-native"
+import{
+    NativeBaseProvider,
+    Box,
+    HStack,
+    Text,
+    VStack,
+    Image,
+}from 'native-base';
+import { TouchableOpacity,ScrollView } from "react-native";
+const Width = Dimensions.get('window').width;
+const Height = Dimensions.get('window').height;
 
+export default function coupon({navigation}){
+        const pressHandler=()=>{
+            navigation.navigate('home_activity');
+        }
+       
+    return (
+        <NativeBaseProvider>
+            <Box 
+            style={{
+                width:Width,
+                height:64,
+                marginTop:40,
+                justifyContent:'center',
+                alignItems:'center',
+            }}>
+                <Text style={{
+                    textAlign:'center',
+                    height:40,
+                    padding:10,
+                    fontSize:24,
+                    fontWeight:'bold',
+                   
+                }}>쿠폰
+            </Text>
+            </Box>
+        </NativeBaseProvider>
+    )
+}
--- Extreme/extreme/routes/home_navigation.js
+++ Extreme/extreme/routes/home_navigation.js
@@ -4,9 +4,9 @@ import { createAppContainer } from "react-navigation";
 import home_region from "../home_region";
 import home_recommend from "../home_recommend";
 import home_activity from "../home_activity";
-import my_page from "../my_page";
 import coupon from "../coupon";
-import heart from "../heart";
+//import heart from "../heart";
+//import my_page from "../my_page";
 
 const screens = {
     home_region: {
@@ -21,18 +21,18 @@ const screens = {
         screen: home_activity,
         navigationOptions: () => ({ headerShown: false, })
     },
-    my_page: {
-        screen: my_page,
-        navigationOptions: () => ({ headerShown: false, })
-    },
     coupon: {
-        screen: coupon,
+         screen: coupon,
         navigationOptions: () => ({ headerShown: false, })
-    },
-    heart: {
-        screen: heart,
-        navigationOptions: () => ({ headerShown: false, })
-    }
+     },
+    // heart: {
+    //     screen: heart,
+    //     navigationOptions: () => ({ headerShown: false, })
+    // },
+    // my_page: {
+    //     screen: my_page,
+    //     navigationOptions: () => ({ headerShown: false, })
+    // },
 }
 
 const trans_stack = createStackNavigator(screens);
