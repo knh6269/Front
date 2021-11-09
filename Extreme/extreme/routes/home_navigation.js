95b00abdb39731bba6df7bd419e80e31b7eb4b74
knh6269 <knh6269@gmail.com>
2021-11-09 13:02:39 +0900

    11.09

--- Extreme/extreme/routes/home_navigation.js
+++ Extreme/extreme/routes/home_navigation.js
@@ -2,16 +2,16 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
 import Home_region from "../home_region";
-import main from "../main";
+import Home_activity from "../home_activity";
 const screens={
    Home_region:{
        screen:Home_region,
        navigationOptions:()=>({headerShown:false,})
    },
-  main:{
-       screen:main,
-       navigationOptions:()=>({headerShown:false,})
-   }
+    Home_activity:{
+        screen:Home_activity,
+        navigationOptions:()=>({headerShown:false,})
+    }
 }
 
 const trans_stack=createStackNavigator(screens);
