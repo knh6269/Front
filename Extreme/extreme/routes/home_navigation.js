be7253be42f872f7860e4a9e0e9d6d3dc21b8c24
qor4 <qor_4@naver.com>
2021-11-09 13:36:57 +0900

    수정

--- Extreme/extreme/routes/home_navigation.js
+++ Extreme/extreme/routes/home_navigation.js
@@ -2,32 +2,21 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
 import Home_region from "../home_region";
-<<<<<<< HEAD
 import Home_recommend from "../Home_recommend";
-import main from "../main";
-=======
 import Home_activity from "../home_activity";
->>>>>>> 95b00abdb39731bba6df7bd419e80e31b7eb4b74
 const screens={
    Home_region:{
        screen:Home_region,
        navigationOptions:()=>({headerShown:false,})
    },
-<<<<<<< HEAD
    Home_recommend:{
     screen:Home_recommend,
     navigationOptions:()=>({headerShown:false,})
 },
-  main:{
-       screen:main,
-       navigationOptions:()=>({headerShown:false,})
-   }
-=======
     Home_activity:{
         screen:Home_activity,
         navigationOptions:()=>({headerShown:false,})
     }
->>>>>>> 95b00abdb39731bba6df7bd419e80e31b7eb4b74
 }
 
 const trans_stack=createStackNavigator(screens);
