efb7c7af5ebc8d777ed33e04d72550441a8d1f28
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2021-11-10 09:27:44 +0900

    cart.js까지 추가

--- Extreme/extreme/cart.js
+++ Extreme/extreme/cart.js
@@ -9,8 +9,8 @@ import{
     Text,
     VStack,
     Image,
-}from 'native-base';
-import { TouchableOpacity,ScrollView } from "react-native";
+} from 'native-base';
+import { TouchableOpacity,ScrollView, StyleSheet } from "react-native";
 const Width = Dimensions.get('window').width;
 const Height = Dimensions.get('window').height;
 
@@ -22,191 +22,157 @@ export default function cart({navigation}){
     return (
         <NativeBaseProvider>
             <ScrollView>
-                <Box marginTop={100} marginLeft={'10%'}>
-                    <HStack space={5}>
-                        <TouchableOpacity onPress={pressHandler}>
-                            <HStack>
-                                <Image
-                                    source={{
-                                        uri: 'https://wallpaperaccess.com/full/317501.jpg',
-                                    }}
-                                    style={{ width: '50%', height:100, }}
-                                    alt="trans_1"
-                                
-                                />
-                            </HStack>
-                            <Box width={'50%'}>
-                                <Text fontSize={10} marginTop={2} style={{fontWeight:'bold', textAlign:'center'}} >image</Text>
-                            </Box>
-                        </TouchableOpacity>
+                <Box>
+                    <Box height={100}>
+                        <Image
+                            source={{
+                                uri: 'https://wallpaperaccess.com/full/317501.jpg',
+                            }}
+                            style={{ marginLeft:'40%', }}
+                                alt="trans_1"
+                        />
+                        <Text fontSize={10} style={{fontWeight:'bold', textAlign:'center'}} >장바구니</Text>
+                    </Box>
+                    <Box marginLeft={'5%'}>
+                        <HStack>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.text}>V</Text>
+                            </TouchableOpacity>
+                            <Image
+                                source={{
+                                    uri: 'https://wallpaperaccess.com/full/317501.jpg',
+                                }}
+                                style={{ marginLeft:'5%', width:'80%', height:30}}
+                                alt="trans_1"/>
+                        </HStack>
+                    </Box>
+                    <Box marginLeft={'18%'}>
+                         <HStack space={150}>
+                            <Text borderWidth={1} fontSize={10} style={{fontWeight:'bold'}} >날짜</Text>
+                            <Text borderWidth={1} fontSize={10} style={{ fontWeight: 'bold' }} >날씨</Text>
+                        </HStack>
+                    </Box>
+                    <Box marginLeft={'18%'}>
+                         <HStack space={116}>
+                            <Text borderWidth={1} fontSize={10} style={{fontWeight:'bold'}} >2021-11-11</Text>
+                            <Text borderWidth={1} fontSize={10} style={{ fontWeight: 'bold' }} >쨍쨍</Text>
+                        </HStack>
+                    </Box>
+                    <Box marginLeft={'18%'} width={'40%'} marginTop={'5%'}>
+                        <HStack space={116}>
+                            <Text borderWidth={1} fontSize={10} style={{fontWeight:'bold'}} >총 결제 금액</Text>
+                            <Text borderWidth={1} fontSize={10} style={{ fontWeight: 'bold' }} >10000원</Text>
+                        </HStack>
+                    </Box>
+                    <Box marginLeft={'18%'} marginTop={'5%'} >
+                        <HStack space={60}>
+                            <TouchableOpacity style={styles.modifydatebutton}>
+                                <Text style={styles.modifydate}>날짜 변경</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.gobuybutton}>
+                                <Text style={styles.gobuy}>날짜 변경</Text>
+                            </TouchableOpacity>
+                        </HStack>
+                    </Box>
+                </Box>
 
-                        <TouchableOpacity onPress={pressHandler}>
-                            <HStack>
-                                <Image
-                                    source={{
-                                        uri: 'https://wallpaperaccess.com/full/317501.jpg',
-                                    }}
-                                    style={{ width: '50%', height:100, }}
-                                    alt="trans_1"
-                                
-                                />
-                            </HStack>
-                            <Box width={'50%'}>
-                                <Text fontSize={10} marginTop={2} style={{fontWeight:'bold', textAlign:'center'}} >image</Text>
-                            </Box>
-                        </TouchableOpacity>
+                <Box marginTop={'5%'}>
+                    <Box marginLeft={'5%'}>
+                        <HStack>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.text}>V</Text>
+                            </TouchableOpacity>
+                            <Image
+                                source={{
+                                    uri: 'https://wallpaperaccess.com/full/317501.jpg',
+                                }}
+                                style={{ marginLeft:'5%', width:'80%', height:30}}
+                                alt="trans_1"/>
+                        </HStack>
+                    </Box>
+                    
+                    <Box marginLeft={'18%'}>
+                         <HStack space={150}>
+                            <Text borderWidth={1} fontSize={10} style={{fontWeight:'bold'}} >날짜</Text>
+                            <Text borderWidth={1} fontSize={10} style={{ fontWeight: 'bold' }} >날씨</Text>
+                        </HStack>
+                    </Box>
+                    <Box marginLeft={'18%'}>
+                         <HStack space={116}>
+                            <Text borderWidth={1} fontSize={10} style={{fontWeight:'bold'}} >2021-11-11</Text>
+                            <Text borderWidth={1} fontSize={10} style={{ fontWeight: 'bold' }} >쨍쨍</Text>
+                        </HStack>
+                    </Box>
+                    <Box marginLeft={'18%'} width={'40%'} marginTop={'5%'}>
+                        <HStack space={116}>
+                            <Text borderWidth={1} fontSize={10} style={{fontWeight:'bold'}} >총 결제 금액</Text>
+                            <Text borderWidth={1} fontSize={10} style={{ fontWeight: 'bold' }} >10000원</Text>
+                        </HStack>
+                    </Box>
+                    <Box marginLeft={'18%'} marginTop={'5%'} >
+                        <HStack space={60}>
+                            <TouchableOpacity style={styles.modifydatebutton}>
+                                <Text style={styles.modifydate}>날짜 변경</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.gobuybutton}>
+                                <Text style={styles.gobuy}>날짜 변경</Text>
+                            </TouchableOpacity>
+                        </HStack>
+                    </Box>
+                </Box>
 
+                <Box marginTop={'10%'}>
+                    <HStack marginLeft={'5%'}>
+                        <VStack>
+                            <Text fontSize={10} style={{fontWeight:'bold'}} >총 결제 금액</Text>
+                            <Text fontSize={10} style={{fontWeight:'bold'}} >10000원</Text>
+                        </VStack>
+                        <Text fontSize={10} style={{ fontWeight: 'bold' }} >(2)</Text>
+                        <TouchableOpacity style={styles.buybutton}>
+                            <Text style={styles.buy}>전체 구매하기</Text>
+                        </TouchableOpacity>
                     </HStack>
                 </Box>
 
-                <Box marginTop={30} marginLeft={'10%'}>
-                    <HStack space={5}>
-                        <TouchableOpacity onPress={pressHandler}>
-                            <HStack>
-                                <Image
-                                    source={{
-                                        uri: 'https://wallpaperaccess.com/full/317501.jpg',
-                                    }}
-                                    style={{ width: '50%', height:100, }}
-                                    alt="trans_1"
-                                
-                                />
-                            </HStack>
-                            <Box width={'50%'}>
-                                <Text fontSize={10} marginTop={2} style={{fontWeight:'bold', textAlign:'center'}} >image</Text>
-                            </Box>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={pressHandler}>
-                            <HStack>
-                                <Image
-                                    source={{
-                                        uri: 'https://wallpaperaccess.com/full/317501.jpg',
-                                    }}
-                                    style={{ width: '50%', height:100, }}
-                                    alt="trans_1"
-                                
-                                />
-                            </HStack>
-                            <Box width={'50%'}>
-                                <Text fontSize={10} marginTop={2} style={{fontWeight:'bold', textAlign:'center'}} >image</Text>
-                            </Box>
-                        </TouchableOpacity>
-
-                    </HStack>
-                </Box>
-
-                <Box marginTop={30} marginLeft={'10%'}>
-                    <HStack space={5}>
-                        <TouchableOpacity onPress={pressHandler}>
-                            <HStack>
-                                <Image
-                                    source={{
-                                        uri: 'https://wallpaperaccess.com/full/317501.jpg',
-                                    }}
-                                    style={{ width: '50%', height:100, }}
-                                    alt="trans_1"
-                                
-                                />
-                            </HStack>
-                            <Box width={'50%'}>
-                                <Text fontSize={10} marginTop={2} style={{fontWeight:'bold', textAlign:'center'}} >image</Text>
-                            </Box>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={pressHandler}>
-                            <HStack>
-                                <Image
-                                    source={{
-                                        uri: 'https://wallpaperaccess.com/full/317501.jpg',
-                                    }}
-                                    style={{ width: '50%', height:100, }}
-                                    alt="trans_1"
-                                
-                                />
-                            </HStack>
-                            <Box width={'50%'}>
-                                <Text fontSize={10} marginTop={2} style={{fontWeight:'bold', textAlign:'center'}} >image</Text>
-                            </Box>
-                        </TouchableOpacity>
-
-                    </HStack>
-                </Box>
-
-                <Box marginTop={30} marginLeft={'10%'}>
-                    <HStack space={5}>
-                        <TouchableOpacity onPress={pressHandler}>
-                            <HStack>
-                                <Image
-                                    source={{
-                                        uri: 'https://wallpaperaccess.com/full/317501.jpg',
-                                    }}
-                                    style={{ width: '50%', height:100, }}
-                                    alt="trans_1"
-                                
-                                />
-                            </HStack>
-                            <Box width={'50%'}>
-                                <Text fontSize={10} marginTop={2} style={{fontWeight:'bold', textAlign:'center'}} >image</Text>
-                            </Box>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={pressHandler}>
-                            <HStack>
-                                <Image
-                                    source={{
-                                        uri: 'https://wallpaperaccess.com/full/317501.jpg',
-                                    }}
-                                    style={{ width: '50%', height:100, }}
-                                    alt="trans_1"
-                                
-                                />
-                            </HStack>
-                            <Box width={'50%'}>
-                                <Text fontSize={10} marginTop={2} style={{fontWeight:'bold', textAlign:'center'}} >image</Text>
-                            </Box>
-                        </TouchableOpacity>
-
-                    </HStack>
-                </Box>
-
-                <Box marginTop={30} marginLeft={'10%'}>
-                    <HStack space={5}>
-                        <TouchableOpacity onPress={pressHandler}>
-                            <HStack>
-                                <Image
-                                    source={{
-                                        uri: 'https://wallpaperaccess.com/full/317501.jpg',
-                                    }}
-                                    style={{ width: '50%', height:100, }}
-                                    alt="trans_1"
-                                
-                                />
-                            </HStack>
-                            <Box width={'50%'}>
-                                <Text fontSize={10} marginTop={2} style={{fontWeight:'bold', textAlign:'center'}} >image</Text>
-                            </Box>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={pressHandler}>
-                            <HStack>
-                                <Image
-                                    source={{
-                                        uri: 'https://wallpaperaccess.com/full/317501.jpg',
-                                    }}
-                                    style={{ width: '50%', height:100, }}
-                                    alt="trans_1"
-                                
-                                />
-                            </HStack>
-                            <Box width={'50%'}>
-                                <Text fontSize={10} marginTop={2} style={{fontWeight:'bold', textAlign:'center'}} >image</Text>
-                            </Box>
-                        </TouchableOpacity>
-
-                    </HStack>
-                </Box>
             </ScrollView>
         </NativeBaseProvider>
     )
 }
+
+const styles = StyleSheet.create({
+
+    button: {
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 30,
+        height: 30,
+        marginBottom: 30,
+        borderRadius: 35,
+    },
+    modifydatebutton: {
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 110,
+        height: 30,
+        borderRadius: 35,
+    },
+    gobuybutton: {
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 110,
+        height: 30,
+        borderRadius: 35,
+    },
+    buybutton: {
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 110,
+        height: 30,
+        borderRadius: 35,
+        marginLeft: 158
+    }
+});
--- Extreme/extreme/routes/home_navigation.js
+++ Extreme/extreme/routes/home_navigation.js
@@ -4,8 +4,9 @@ import home_region from "../home_region";
 import home_recommend from "../home_recommend";
 import home_activity from "../home_activity";
 import coupon from "../coupon";
-//import heart from "../heart";
-//import my_page from "../my_page";
+import heart from "../heart";
+import my_page from "../my_page";
+import cart from "../cart"
 
 const screens = {
    
@@ -24,14 +25,18 @@ const screens = {
         screen: coupon,
         navigationOptions: () => ({ headerShown: false, })
     },
-    // heart: {
-    //     screen: heart,
-    //     navigationOptions: () => ({ headerShown: false, })
-    // },
-    // my_page: {
-    //     screen: my_page,
-    //     navigationOptions: () => ({ headerShown: false, })
-    // },
+    heart: {
+        screen: heart,
+        navigationOptions: () => ({ headerShown: false, })
+    },
+    my_page: {
+        screen: my_page,
+        navigationOptions: () => ({ headerShown: false, })
+    },
+    cart: {
+        screen: cart,
+        navigationOptions: () => ({ headerShown: false, })
+    }
 }
 
 const trans_stack = createStackNavigator(screens);
