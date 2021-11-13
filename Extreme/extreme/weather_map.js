3212119662cbfd6eb2ea1bc5d3d28b1229342ec3
knh6269 <knh6269@gmail.com>
2021-11-14 00:43:32 +0900

    맵 커스텀 마커 추가

--- Extreme/extreme/weather_map.js
+++ Extreme/extreme/weather_map.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Styled from 'styled-components/native';
 import MapView ,{PROVIDER_GOOGLE} from 'react-native-maps';
-import {} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 import { NativeBaseProvider,Box,Text } from 'native-base';
 import {
   Dimensions,} from "react-native";
@@ -11,23 +11,61 @@ const Container = Styled.View`
 `;
 const Width = Dimensions.get('window').width;
 const Height = Dimensions.get('window').height;
+let des="낙현이집";
 const AppleMap = () => {
+ 
   return (
     <NativeBaseProvider>
-    <Box style={{height:40}}><Text>내 동네 지도</Text></Box>
+         <Box 
+            style={{
+                width:Width,
+                height:Height/12,
+                marginTop:40,
+                justifyContent:'center',
+                alignItems:'center',
+                borderWidth:3,
+                marginBottom:30,
+            }}>
+                <Text style={{
+                    textAlign:'center',
+                    height:Height/19,
+                    padding:10,
+                    fontSize:24,
+                    fontWeight:'bold',
+                }}>날씨 지도
+            </Text>
+            </Box>
+    
     <Box style={{height:Height/2}}> 
       <Container>
-        <MapView style={{flex: 1}} provider={PROVIDER_GOOGLE} 
+        <MapView style={{flex: 1,backgroundColor:'red'}} provider={PROVIDER_GOOGLE} 
           initialRegion={{
             latitude: 36.62917303057579, 
             longitude: 127.45632936923715,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }} />
+            latitudeDelta: 3,
+            longitudeDelta: 3,
+            
+          }} >
+             <MapView.Marker
+            coordinate={{latitude:36.62917303057579, 
+            longitude: 127.45632936923715}}
+            image={require('./images/그림7.png')}
+            title={"청주"}
+            description={des}
+         />
+         
+         <MapView.Marker
+            coordinate={{latitude:35.5, 
+            longitude: 127.45632936923715}}
+            image={require('./images/그림8.png')}
+            title={"청주"}
+            description={des}
+         />
+          </MapView>
       </Container>
     </Box>
     <Box>
-      <Text>asssss</Text>
+      <Text>ax</Text>
     </Box>
     </NativeBaseProvider>
   );
